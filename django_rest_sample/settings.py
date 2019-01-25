84659ef7c1d142673c3f9005c0c6f0ddcc4582a6
owlbeck <owlbeck@yahoo.com>
2019-01-25 12:00:43 +0900

    :+1:APIのページネーションに対応した fix #8

--- django_rest_sample/settings.py
+++ django_rest_sample/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django REST framework
+# https://www.django-rest-framework.org/
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
+}
